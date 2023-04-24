ae1a9eb4ce4691924035a1c3d91b661d7fc772ae
Aarushi3113 <aarug3113#gmail.com>
2023-04-24 19:20:05 -0400

    Extracting info from AS path

--- main.py
+++ main.py
@@ -21,3 +21,33 @@ stream = bgp.BGPStream(
     record_type="updates",
     filter="prefix more 210.180.0.0/16"
 )
+
+
+# Creating a dictionary 
+# to store the AS paths and origins
+as_paths = {}
+
+# Looping through each element in the stream
+for elem in stream:
+
+    # Extracting the as-path
+    as_path = elem.fields['as-path'].split(' ')
+    # Extracting the origin AS
+    origin_as = as_path[-1]
+    # Extracting the peer AS
+    peer_as = elem.peer_asn
+    # Extracting the prefix
+    prefix = elem.fields['prefix']
+    # Extract the timestamp
+    timestamp = elem.time
+
+    # Updating the AS paths dictionary
+    if prefix in as_paths:
+        as_paths[prefix].append(as_path)
+    else:
+        as_paths[prefix] = [as_path]
+
+    # Printing the information
+    print('Update to AS {0} from AS {1} for prefix {2} at {3}:'.format(peer_as, origin_as, prefix, timestamp))
+    print('AS Path: {0}'.format(as_path))
+    print('')
