0e75dc9a42b68039adf26d8268c90c2e8042353e
AkalyaAsokan <akalyaasokan@gmail.com>
2023-04-30 08:55:59 -0400

    Update visualizations.py

--- visualizations.py
+++ visualizations.py
@@ -1,87 +1,165 @@
+# To plot graph
 import networkx as nx
 import matplotlib.pyplot as plt
-from collections import Counter
-from collections import defaultdict
-# Creating a NetworkX graph object
-G = nx.Graph()
-as_paths = {}
+# To AS information from an AS Number
+import json
+import requests
+# To read from files
+import os
 
-# Open the file in read mode
-with open('training_data.txt', 'r') as f:
-    lines = f.readlines()
+class Visualize:
+    """
+    Visualize class that collects stats and visualise components of the training data
+    """
 
-# Parse the lines and create the dictionary
-for i in range(len(lines)):
-    if lines[i].startswith('Prefix: '):
-        prefix = lines[i].strip().split(' ')[1]
-        as_paths[prefix] = []
-        for j in range(i+2, len(lines)):
-            if lines[j].startswith('AS path: '):
-                as_path = lines[j].split(': ')[-1].strip().split(' -> ')
-                as_paths[prefix].append(as_path)
+    def __init__(self):
+        # Dictionary to store the prefixes with multiple origin ASes
+        self.lines = {}
+
+        # Creating a NetworkX graph object
+        self.G = nx.Graph()
+
+        # Creating a dictionary 
+        # to store the AS paths and origins
+        self.as_paths = {}
+
+        # Dictionary storing AS Names
+        self.as_names = {}
+
+    def read_from_file(self):
+        # Define the base filename
+        base_filename = 'training_data_'
+
+        # Define the list to store all lines from all files
+        all_lines = []
+
+        # Initialize the file number to 1
+        file_num = 1
+
+        while True:
+            # Construct the filename for this iteration
+            filename = base_filename + str(file_num).zfill(2) + '.txt'
+            #filename = 'training_data_042820231148.txt'
+
+            # Check if the file exists
+            if os.path.isfile(filename):
+                # Open the file and read its contents
+                with open(filename, 'r') as f:
+                    lines = f.readlines()
+
+                # Add the lines to the list of all lines
+                all_lines.extend(lines)
+
+                # Increment the file number
+                file_num += 1
             else:
+                # If the file doesn't exist, exit the loop
                 break
 
-# Print the dictionary
-# print(as_paths)
-# Printing the AS paths for each prefix
-for prefix, paths in as_paths.items():
-    print(paths)
-    # Converting inner lists to tuples
-    paths = [tuple(path) for path in paths]
-    # Adding nodes for the origin ASes
-    origin_ases = set([path[-1] for path in paths])
-    for as_node in origin_ases:
-        G.add_node(as_node)
+        return all_lines
+    
+    def get_asn_name(self, asn):
+        # API to get AS information from an AS Number
+        response = requests.get(f"https://stat.ripe.net/data/as-names/data.json?resource=AS{asn}")
+        if response.ok:
+            data = json.loads(response.text)
+            
+            # Return the AS Name
+            return data["data"]["names"][str(asn)]
+        else:
+            return "Error fetching ASN information"
 
-    # Adding edges between ASes that appear in the same path
-    for path in paths:
-        src = path[0]
-        dest = path[len(path) - 1]
-        G.add_edge(src, dest, weight=len(path))
+    def start(self):
 
-# Calculating and printing basic graph metrics
-print("Number of nodes:", G.number_of_nodes())
-print("Number of edges:", G.number_of_edges())
-print("Average degree:", sum(dict(G.degree()).values()) / G.number_of_nodes())
+        self.lines = self.read_from_file()
 
-# Drawing the graph
-pos = nx.spring_layout(G)
-nx.draw_networkx(G, pos, node_size=400, font_size=10)
-edge_labels = {(u, v): str(G[u][v]['weight']) for u, v in G.edges()}
-nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=8)
-plt.show()
+        # Parse the lines and create the dictionary
+        for i in range(len(self.lines)):
+            if self.lines[i].startswith('Prefix: '):
+                prefix = self.lines[i].strip().split(' ')[1]
+                if prefix not in self.as_paths:
+                    self.as_paths[prefix] = []
+                for j in range(i+2, len(self.lines)):
+                    if self.lines[j].startswith('AS path: '):
+                        self.as_path = self.lines[j].split(': ')[-1].strip().split(' -> ')
+                        self.as_paths[prefix].append(self.as_path)
+                    else:
+                        break
 
-as_paths = []
-for i in range(len(lines)):
-    if lines[i].startswith('Update to '):
-        as_paths.append(lines[i+1].split('AS Path: ')[1].strip("[").strip("]\n").replace("'", "").split(", "))
+        # Printing the AS paths for each prefix
+        for prefix, paths in self.as_paths.items():
+            # Converting inner lists to tuples
+            paths = [tuple(path) for path in paths]
+            # Adding nodes for the origin ASes
+            origin_ases = set([path[-1] for path in paths])
+            for as_node in origin_ases:
+                if as_node not in self.as_names:
+                    as_name = self.get_asn_name(as_node)
+                    self.G.add_node(as_name)
+                    self.as_names[as_node] = as_name
+                else:
+                    self.G.add_node(self.as_names[as_node])
 
-print(len(as_paths))
-# Count frequency of each ASN in AS paths
-# Count the number of times the origin prefix has repeated itself in an AS path
-origin_prefix_count = [path.count(path[-1]) for path in as_paths]
-count = {}
-for i in range(len(as_paths)):
-    count[as_paths[i][-1]] = []
-for i in range(len(as_paths)):
-    count[as_paths[i][-1]].append(as_paths[i].count(as_paths[i][-1]))
+            # Adding edges between ASes that appear in the same path
+            for path in paths:
+                if path[0] not in self.as_names:
+                    src = self.get_asn_name(path[0])
+                    self.G.add_node(src)
+                    self.as_names[path[0]] = src
+                else:
+                    src = self.as_names[path[0]]
+                    self.G.add_node(src)
 
-print(count)
+                if path[0] not in self.as_names:
+                    dest = self.get_asn_name(path[len(path) - 1])
+                    self.G.add_node(dest)
+                    self.as_names[path[len(path) - 1]] = dest
+                else:
+                    dest = self.as_names[path[len(path) - 1]]
+                    self.G.add_node(dest)
 
-count1 = sum(1 for values in count.values() if len(set(values)) > 1)
-max_values = {key: max(values) for key, values in count.items()}
+                self.G.add_edge(src, dest, weight=len(path))
 
-fig, ax2 = plt.subplots()
-# Bar chart for max value for each key
-ax2.bar(max_values.keys(), max_values.values(), color='tab:orange')
-ax2.set_title('Max value for each key')
-ax2.set_xlabel('Key')
-ax2.set_ylabel('Max Value')
-ax2.tick_params(axis='x', labelrotation=45)
+        # Calculating and printing basic graph metrics
+        print("Number of nodes:", self.G.number_of_nodes())
+        print("Number of edges:", self.G.number_of_edges())
+        print("Average degree:", sum(dict(self.G.degree()).values()) / self.G.number_of_nodes())
 
-# Set title for the figure
-fig.suptitle('Multi-series bar chart')
+        # Drawing the graph
+        pos = nx.spring_layout(self.G)
+        nx.draw_networkx(self.G, pos, node_size=400, font_size=10)
+        edge_labels = {(u, v): str(self.G[u][v]['weight']) for u, v in self.G.edges()}
+        nx.draw_networkx_edge_labels(self.G, pos, edge_labels=edge_labels, font_size=8)
+        plt.show()
 
-# Display the plot
-plt.show()
+        as_paths = []
+        for i in range(len(self.lines)):
+            if self.lines[i].startswith('Update to '):
+                as_paths.append(self.lines[i+1].split('AS Path: ')[1].strip("[").strip("]\n").replace("'", "").split(", "))
+
+        # Count frequency of each ASN in AS paths
+        # Count the number of times the origin prefix has repeated itself in an AS path
+        # origin_prefix_count = [path.count(path[-1]) for path in as_paths]
+        count = {}
+        for i in range(len(as_paths)):
+            count[as_paths[i][-1]] = []
+        for i in range(len(as_paths)):
+            count[as_paths[i][-1]].append(as_paths[i].count(as_paths[i][-1]))
+
+        # Find the maximum value in the dictionary
+        max_val = max([max(count[key]) for key in count])
+
+        # Create a list of counts for each index for each key
+        counts = {key: [0] * (max_val+1) for key in count}
+        fig, ax = plt.subplots()
+        for asn in count:
+            for val in count[asn]:
+                counts[asn][val] += 1
+            ax.plot(range(2,max_val+1), counts[asn][2:], label=self.get_asn_name(asn))
+
+        ax.set_xlabel('X Axis')
+        ax.set_ylabel('Y Axis')
+        ax.set_xticks(range(2,max_val+1))
+        #ax.set_xticklabels(range(min_val, max_val+1))
+        ax.legend()
+        plt.show()
