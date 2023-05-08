4bc3f86acb1b08e976720490ded3dad128edacbc
Sadman Chowdhury <64035273+SAD2023@users.noreply.github.com>
2023-05-08 18:08:52 -0400

    created a connected graph for the previous paths

--- fake_path_detector.py
+++ fake_path_detector.py
@@ -1,76 +1,71 @@
 import os
-import re
-import pybgpstream as bgp
+import pybgpstream
+from collections import defaultdict
+import networkx as nx
 
 class FakePathDetector:
-    def __init__(self, collectors, time_from, time_to, record_type, filter, training_files):
-        # Dictionary to store the prefixes with multiple origin ASes
-        self.prefixes = {}
-        self.established_paths = self.read_training_data(training_files)
+    def __init__(self):
+        self.graphs = defaultdict(nx.Graph)
+        self.origin_as = defaultdict(int)
 
-        self.stream = bgp.BGPStream(
-            from_time=time_from, until_time=time_to,
-            collectors=collectors,
-            record_type=record_type,
-            filter=filter
+    def train(self, training_files):
+        for file in training_files:
+            with open(file, "r") as f:
+                for line in f:
+                    if '|' not in line:
+                        continue
+                    
+                    prefix, path = line.strip().split("|")
+                    path = tuple(map(int, path.split()))
+                    origin = path[-1]
+
+                    #update graph
+                    self.graphs[prefix].add_edges_from(zip(path[:-1], path[1:]))
+
+                    # update origin AS
+                    self.origin_as[prefix] = origin
+
+    def is_fake_path(self, path, prefix):
+        graph = self.graphs[prefix]
+
+        # check whether path is already connected in the graph
+        for i in range(len(path) - 1):
+            if not graph.has_edge(path[i], path[i + 1]):
+                break
+        else:
+            return False
+
+        # check if origin AS is the same
+        return path[-1] == self.origin_as[prefix]
+
+    def collect_bgp_data(self):
+        # specify 
+        stream = pybgpstream.BGPStream(
+            from_time="2017-07-07 00:00:00",
+            until_time="2017-07-08 00:00:00 UTC",
+            collectors=["route-views.sg", "route-views.eqix"],
+            filter="prefix more 210.0.0.0/08",
+            record_type="updates",
         )
 
-        # Looping through each element in the stream
-        for elem in self.stream:
-            self.detect_fake_as_paths(elem)
+        # processing updates
+        for elem in stream:
+            # filter by announcement
+            if elem.type == "A":
+                # get prefix + path
+                prefix = elem.fields["prefix"]
+                path = tuple(map(int, elem.fields["as-path"].split()))
 
-    def read_training_data(self, training_files):
-      established_paths = {}
-
-      for file in training_files:
-          with open(file, 'r') as f:
-              lines = f.readlines()
-              for line in lines:
-                  if "AS Path:" in line:
-                      prefix_line = lines[lines.index(line) - 1]
-                      prefix = re.search(r'for prefix (.+?) at', prefix_line).group(1)
-
-                      as_path_match = re.search(r'\[(.*)\]', line)
-                      if as_path_match:
-                          as_path = as_path_match.group(1).split(', ')
-                      else:
-                          continue
-
-                      if prefix not in established_paths:
-                          established_paths[prefix] = []
-
-                      established_paths[prefix].append(as_path)
-
-      return established_paths
-
-    def detect_fake_as_paths(self, elem):
-        try:
-            prefix = elem.fields["prefix"]
-            as_path = elem.fields["as-path"].split()
-
-            # If the prefix is not in the established paths, it's a new prefix
-            if prefix not in self.established_paths:
-                print(f"New prefix detected: {prefix}")
-                return
-
-            # If the AS path is not in the established paths for this prefix, it's a fake path
-            if as_path not in self.established_paths[prefix]:
-                print(f"Fake AS path detected for prefix {prefix}: {as_path}")
-
-        except KeyError:
-            # Ignore elements that don't have prefix or AS path fields
-            pass
-
-training_files = ["training_data_01 .txt", "training_data_02.txt", "training_data_03.txt", "training_data_04.txt", "training_data_05.txt", "training_data_06.txt"] 
-
-fake = FakePathDetector(
-    collectors=["route-views.sg", "route-views.eqix"],
-    time_from="2017-04-08 00:00:00",
-    time_to="2017-04-15 00:00:00 UTC",
-    record_type="updates",
-    filter="prefix more 210.0.0.0/16",
-    training_files=training_files
-)
+                # check if path is fake
+                if self.is_fake_path(path, prefix):
+                    print(f"Fake path detected for prefix {prefix}: {path}")
 
 
+if __name__ == "__main__":
+    training_files = [
+        'training_data_12.txt', 'training_data_11.txt', 'training_data_10.txt'
+    ]
 
+    detector = FakePathDetector()
+    detector.train(training_files)
+    detector.collect_bgp_data()
