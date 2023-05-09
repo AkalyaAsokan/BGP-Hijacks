1c728adb445663d7d08237a245886add08a6c4dd
AkalyaAsokan <akalyaasokan@gmail.com>
2023-05-09 03:39:17 -0400

    Update visualizations.py

--- visualizations.py
+++ visualizations.py
@@ -1,10 +1,17 @@
-# To plot graph
+
+# Importing networkx, a Python library used for complex network analysis and visualization
 import networkx as nx
+
+# Importing matplotlib, a Python library used for data visualization
 import matplotlib.pyplot as plt
-# To AS information from an AS Number
+
+# Importing json, a Python library used for getting AS Information using RIPE Stat API
 import json
+
+# Importing requests, a Python library used for getting AS Information using RIPE Stat API
 import requests
-# To read from files
+
+# Importing os, to read from files
 import os
 
 class Visualize:
@@ -13,6 +20,7 @@ class Visualize:
     """
 
     def __init__(self):
+
         # Dictionary to store the prefixes with multiple origin ASes
         self.lines = {}
 
@@ -27,8 +35,9 @@ class Visualize:
         self.as_names = {}
 
     def read_from_file(self):
+
         # Define the base filename
-        base_filename = 'training_data_'
+        base_filename = 'training_data_2017'
 
         # Define the list to store all lines from all files
         all_lines = []
@@ -38,7 +47,7 @@ class Visualize:
 
         while True:
             # Construct the filename for this iteration
-            filename = base_filename + str(file_num).zfill(2) + '.txt'
+            filename = base_filename + '.txt'
             #filename = 'training_data_042820231148.txt'
 
             # Check if the file exists
@@ -59,11 +68,12 @@ class Visualize:
         return all_lines
     
     def get_asn_name(self, asn):
+
         # API to get AS information from an AS Number
         response = requests.get(f"https://stat.ripe.net/data/as-names/data.json?resource=AS{asn}")
         if response.ok:
             data = json.loads(response.text)
-            
+
             # Return the AS Name
             return data["data"]["names"][str(asn)]
         else:
@@ -130,6 +140,7 @@ class Visualize:
         nx.draw_networkx(self.G, pos, node_size=400, font_size=10)
         edge_labels = {(u, v): str(self.G[u][v]['weight']) for u, v in self.G.edges()}
         nx.draw_networkx_edge_labels(self.G, pos, edge_labels=edge_labels, font_size=8)
+        plt.style.use('fivethirtyeight')
         plt.show()
 
         as_paths = []
@@ -157,9 +168,8 @@ class Visualize:
                 counts[asn][val] += 1
             ax.plot(range(2,max_val+1), counts[asn][2:], label=self.get_asn_name(asn))
 
-        ax.set_xlabel('X Axis')
-        ax.set_ylabel('Y Axis')
+        ax.set_xlabel('Count of AS prepending per route announcement')
+        ax.set_ylabel('Count of number of times the AS has prepended')
         ax.set_xticks(range(2,max_val+1))
-        #ax.set_xticklabels(range(min_val, max_val+1))
         ax.legend()
         plt.show()
